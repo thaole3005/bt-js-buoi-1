694228f34c64788c12955ffc5e6125a58d9792b8
LeThao <thaothao30052000@gmail.com>
2021-07-26 00:09:23 +0700

    bt2

--- js/bt2.js
+++ js/bt2.js
@@ -1,16 +1,26 @@
 /**
  * Khối 1: Input
- * salary1Day
- *workDays
+ * num1
+ * num2
+ * num3
+ * num4
+ * num5
  * 
  * Khối 2: Các bước xử lý
  * B1: Tạo và gán giá trị
- * B2: salary =  salary1Day*workDays
+ * B2: tbc = (num1+num2+num3+num4+num5) / 5
  * 
  * Khối 3: Output
  * 
- * totalSalary
+ * tbc
  * 
  */
 
- 
+ var num1 = 1;
+ var num2 = 2;
+ var num3 = 3;
+ var num4 = 4;
+ var num5 = 5;
+
+ var tbc = (num1 + num2 + num3 + num4 + num5) / 5;
+ console.log('BT2-số trung bình cộng: ' + tbc);
