0dbca0677f442677d9600789320bb3bcaa526931
LeThao <thaothao30052000@gmail.com>
2021-07-25 23:58:41 +0700

    bt1

---
+++ js/bt1.js
@@ -0,0 +1,22 @@
+/**
+ * Khối 1: Input
+ * salary1Day
+ *workDays
+ * 
+ * Khối 2: Các bước xử lý
+ * B1: Tạo và gán giá trị
+ * B2: salary =  salary1Day*workDays
+ * 
+ * Khối 3: Output
+ * 
+ * totalSalary
+ * 
+ */
+
+
+ const salary1Day = 100000;
+ var workDays = 30;
+ var totalSalary = salary1Day * workDays;
+ console.log('BT1-Tổng lương: ' + totalSalary);
+
+ 
---
+++ js/bt2.js
@@ -0,0 +1,16 @@
+/**
+ * Khối 1: Input
+ * salary1Day
+ *workDays
+ * 
+ * Khối 2: Các bước xử lý
+ * B1: Tạo và gán giá trị
+ * B2: salary =  salary1Day*workDays
+ * 
+ * Khối 3: Output
+ * 
+ * totalSalary
+ * 
+ */
+
+ 
