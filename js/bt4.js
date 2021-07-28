a54875966d4d7a4ad950765d1aa8ad431972f191
LeThao <thaothao30052000@gmail.com>
2021-07-28 13:11:15 +0700

    update

--- js/bt4.js
+++ js/bt4.js
@@ -6,6 +6,7 @@
  * Khối 2: Các bước xử lý
  * B1: Tạo và gán giá trị
  * B2: chuVi = ( chieuDai + chieuRong ) * 2
+ *  dienTich =   chieuDai * chieuRong 
  * 
  * Khối 3: Output
  * 
@@ -16,4 +17,5 @@
 var chieuDai = 10;
 var chieuRong = 5;
 var chuVi =  ( chieuDai + chieuRong ) * 2;
-console.log('BT4-kết quả chu vi: ' + chuVi);
+var dienTich =   chieuDai * chieuRong ;
+console.log('BT4-kết quả chu vi, diện tích là: ' + chuVi + '-' + dienTich);
